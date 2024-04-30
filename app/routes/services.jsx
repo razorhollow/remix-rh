b05f73d0ae0aabddc94220c830176ae72ec263c4
RazorHollow <rob@razorhollow.com>
2024-04-30 15:43:31 -0400

    add NavLink to services index

--- app/routes/services.jsx
+++ app/routes/services.jsx
@@ -1,9 +1,10 @@
-import { Outlet } from '@remix-run/react'
+import { Outlet, NavLink } from '@remix-run/react'
 
 export default function Services() {
   return (
-    <div>
+    <div className='flex justify-around'>
       <h1 className=''>Services We Offer</h1>
+      <NavLink to='webdev' className={({ isActive }) => `underline ${isActive ? 'bg-gray-800 text-white' : ''}`}>Web Dev</NavLink>
       <Outlet />
     </div>
   );
