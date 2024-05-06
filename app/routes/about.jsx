598f05e3835053e0f4f0a92a6cbdbde0438cbd55
RazorHollow <rob@razorhollow.com>
2024-05-06 15:38:09 -0400

    add about copy

---
+++ app/routes/about.jsx
@@ -0,0 +1,17 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <p>(Photo Here) Hi there, I'm Rob Reynolds - an experienced outdoor photographer and filmmaker who spent the last 10 years in the outdoor industry. My peers often dub me the "Visual Maestro", and rightfully so, as for a decade, I've been assisting my clients to fill their books with business, creating a compelling online presence through stunning visuals and fully-functional, custom websites.
+
+But why do I do what I do?
+
+Because I've been where you are. I know how daunting it can be to try and market your outdoor business, with no clear starting point and potential customers slipping away due to underwhelming visuals. All the while, wasting time and precious resources.
+
+I started off as a struggling small business owner, hoping that clients would somehow find me through my minimal marketing efforts. It was back when social media was still a budding concept, and online video was unheard of. I had no choice but to figure things out on my own. I taught myself how to build websites and capture great photos, which eventually led me to start a video production and photography company.
+
+That's when I discovered the true power of high-quality visuals and how they can transform a business. A compelling image is not just an image; it's a magnet that draws customers in.
+
+I understand that having a robust online presence with captivating visuals is crucial for your success in the outdoor industry. I understand this challenge very well and that's why I'm dedicated to helping you elevate your brand, connect with your audience visually, and attract more clients effortlessly.</p>
+    </div>
+  );
+}
