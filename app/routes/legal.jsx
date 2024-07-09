a1314bde0d63f6df6fb49b1ea8dbf0f297246429
RazorHollow <rob@razorhollow.com>
2024-07-09 09:45:36 -0400

    reformat legal page to remove multiple h1s

--- app/routes/legal.jsx
+++ app/routes/legal.jsx
@@ -1,65 +1,66 @@
 export default function Legal() {
   return (
-<main className="bg-gray-100 font-sans leading-normal text-gray-800">
-    <div className="container mx-auto p-8">
+    <main className="bg-gray-100 font-sans leading-normal text-gray-800">
+      <div className="container mx-auto p-8">
         <div className="bg-white rounded shadow p-6 m-4">
-            <h1 className="text-2xl font-bold mb-4">Terms of Service</h1>
-            <h2 className="font-semibold text-lg">1. Introduction</h2>
-            <p>Welcome to Razor Hollow, LLC. By accessing our website and using our services, you agree to be bound by these Terms of Service and all applicable laws and regulations. If you disagree with any part of the terms, please do not use our site.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">2. Services</h2>
-            <p>Razor Hollow, LLC provides website development, film production, photography, and marketing strategy services. Our services are available only to entities and individuals within the United States.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">3. Use License</h2>
-            <p>Permission is granted to temporarily download one copy of the materials on Razor Hollow, LLC&apos;s website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license, you may not:</p>
-            <ul className="list-disc ml-8">
-                <li>modify or copy the materials;</li>
-                <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-                <li>attempt to decompile or reverse engineer any software contained on Razor Hollow, LLC&apos;s website;</li>
-                <li>transfer the materials to another person or &quot;mirror&quot; the materials on any other server.</li>
-            </ul>
-            
-            <h2 className="font-semibold text-lg mt-4">4. Disclaimer</h2>
-            <p>The materials on Razor Hollow, LLC&apos;s website are provided on an &apos;as is&apos; basis. Razor Hollow, LLC makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">5. Limitations</h2>
-            <p>In no event shall Razor Hollow, LLC or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Razor Hollow, LLC&apos;s website.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">6. Modifications</h2>
-            <p>Razor Hollow, LLC may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
+          <h1 className="text-2xl font-bold mb-4">Legal Policies</h1>
+          
+          <h2 className="text-xl font-bold mb-4">Terms of Service</h2>
+          <h3 className="font-semibold text-lg">1. Introduction</h3>
+          <p>Welcome to Razor Hollow, LLC. By accessing our website and using our services, you agree to be bound by these Terms of Service and all applicable laws and regulations. If you disagree with any part of the terms, please do not use our site.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">2. Services</h3>
+          <p>Razor Hollow, LLC provides website development, film production, photography, and marketing strategy services. Our services are available only to entities and individuals within the United States.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">3. Use License</h3>
+          <p>Permission is granted to temporarily download one copy of the materials on Razor Hollow, LLC&apos;s website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license, you may not:</p>
+          <ul className="list-disc ml-8">
+            <li>modify or copy the materials;</li>
+            <li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
+            <li>attempt to decompile or reverse engineer any software contained on Razor Hollow, LLC&apos;s website;</li>
+            <li>transfer the materials to another person or &quot;mirror&quot; the materials on any other server.</li>
+          </ul>
+          
+          <h3 className="font-semibold text-lg mt-4">4. Disclaimer</h3>
+          <p>The materials on Razor Hollow, LLC&apos;s website are provided on an &apos;as is&apos; basis. Razor Hollow, LLC makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties including, without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">5. Limitations</h3>
+          <p>In no event shall Razor Hollow, LLC or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption) arising out of the use or inability to use the materials on Razor Hollow, LLC&apos;s website.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">6. Modifications</h3>
+          <p>Razor Hollow, LLC may revise these terms of service for its website at any time without notice. By using this website you are agreeing to be bound by the then current version of these terms of service.</p>
 
-            <h1 className="text-2xl font-bold mb-4 mt-8">Privacy Policy</h1>
-            <h2 className="font-semibold text-lg">1. Introduction</h2>
-            <p>Your privacy is important to us. It is Razor Hollow, LLC&apos;s policy to respect your privacy regarding any information we may collect while operating our website.</p>
+          <h2 className="text-xl font-bold mb-4 mt-8">Privacy Policy</h2>
+          <h3 className="font-semibold text-lg">1. Introduction</h3>
+          <p>Your privacy is important to us. It is Razor Hollow, LLC&apos;s policy to respect your privacy regarding any information we may collect while operating our website.</p>
 
-            <h2 className="font-semibold text-lg mt-4">2. Personal Information</h2>
-            <p>We collect personal information only when you voluntarily provide it through our email list signup amd contact form. This information is used to contact or identify you and includes details such as your email address.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">3. Use of Information</h2>
-            <p>The information you provide is used for internal purposes, such as improving our services, and sending you promotional emails about new products, special offers, or other information we think you may find interesting using the email address you have provided.</p>
+          <h3 className="font-semibold text-lg mt-4">2. Personal Information</h3>
+          <p>We collect personal information only when you voluntarily provide it through our email list signup and contact form. This information is used to contact or identify you and includes details such as your email address.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">3. Use of Information</h3>
+          <p>The information you provide is used for internal purposes, such as improving our services, and sending you promotional emails about new products, special offers, or other information we think you may find interesting using the email address you have provided.</p>
 
-            <h2 className="font-semibold text-lg mt-4">4. Cookies</h2>
-            <p>Currently, our website only collects essential cookies required for the operation of the site, such as session management for our upcoming client login feature. No third-party cookies are used.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">5. Security</h2>
-            <p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic, and managerial procedures to safeguard and secure the information we collect online.</p>
-            
-            <h2 className="font-semibold text-lg mt-4">6. Changes to This Privacy Policy</h2>
-            <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. Changes are effective when they are posted on this page.</p>
+          <h3 className="font-semibold text-lg mt-4">4. Cookies</h3>
+          <p>Currently, our website only collects essential cookies required for the operation of the site, such as session management for our upcoming client login feature. No third-party cookies are used.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">5. Security</h3>
+          <p>We are committed to ensuring that your information is secure. In order to prevent unauthorized access or disclosure, we have put in place suitable physical, electronic, and managerial procedures to safeguard and secure the information we collect online.</p>
+          
+          <h3 className="font-semibold text-lg mt-4">6. Changes to This Privacy Policy</h3>
+          <p>We may update our Privacy Policy from time to time. We will notify you of any changes by posting the new Privacy Policy on this page. Changes are effective when they are posted on this page.</p>
 
-            <h1 className="text-2xl font-bold mb-4 mt-8">Conditions</h1>
-            <p>By using our website, you are considered to have accepted the terms contained within these Terms of Service and Privacy Policy. If you have any questions about these, please contact us at rob@razorhollow.com.</p>
+          <h2 className="text-xl font-bold mb-4 mt-8">Conditions</h2>
+          <p>By using our website, you are considered to have accepted the terms contained within these Terms of Service and Privacy Policy. If you have any questions about these, please contact us at rob@razorhollow.com.</p>
         </div>
-    </div>
-</main>
-
+      </div>
+    </main>
   );
 }
 
-export const meta =() => {
+export const meta = () => {
   return [
-    {title: 'Legal Policies | Privacy & Terms | Razor Hollow'},
-    {name: "description", content: "Read Razor Hollow's legal policies, including our privacy policy and terms of service. Understand how we protect your data and our terms of use."},
+    { title: 'Legal Policies | Privacy & Terms | Razor Hollow' },
+    { name: "description", content: "Read Razor Hollow's legal policies, including our privacy policy and terms of service. Understand how we protect your data and our terms of use." },
     {
       tagName: "link",
       rel: "canonical",
